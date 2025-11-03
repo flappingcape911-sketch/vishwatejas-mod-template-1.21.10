8fde2159d7eb5e5035b137b9866a2f4feb05a532
flappingcape911-sketch <flappingcape911@gmail.com>
2025-11-03 13:01:31 +0530

    this is a test #1

--- src/main/java/net/vishwateja/vishwatejasmod/VishwatejasMod.java
+++ src/main/java/net/vishwateja/vishwatejasmod/VishwatejasMod.java
@@ -12,6 +12,6 @@ public class VishwatejasMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        LOGGER.info("Hello Fabric world! VishwatejasMod has been initialized!");
 	}
 }
