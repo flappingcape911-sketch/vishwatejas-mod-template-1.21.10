e50d85bc8b12c71557d98546b19f7c2db9e012e0
flappingcape911-sketch <flappingcape911@gmail.com>
2025-11-03 12:42:37 +0530

    this is a test #1

---
+++ src/main/java/net/vishwateja/vishwatejasmod/VishwatejasMod.java
@@ -0,0 +1,17 @@
+package net.vishwateja.vishwatejasmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VishwatejasMod implements ModInitializer {
+	public static final String MOD_ID = "vishwatejas-mod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/vishwateja/vishwatejasmod/VishwatejasModClient.java
@@ -0,0 +1,10 @@
+package net.vishwateja.vishwatejasmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VishwatejasModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
